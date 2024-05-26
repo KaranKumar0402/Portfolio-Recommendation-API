2b89ffe58013bdc77e73d589fbe098252b4c61b1
Karan Kumar Singh <skarankumar690@gmail.com>
2024-05-26 09:22:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def recommend():
     df.set_index(pd.DatetimeIndex(df.Date.values), inplace = True)
     df.drop("Date", axis = 1, inplace=True)
 
-    data = request.args.get('year')
+    yr = request.args.get('year')
     if yr == 1:
         df = df['2023-05-22':'2024-05-22']
     elif yr == 3:
